c6f2c9c4f572e065a611fb7430b77884104a9aff
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-07-08 11:53:28 +0100

    Added erray template outline

---
+++ erray/erray_test.go
@@ -0,0 +1,7 @@
+package erray
+/**
+ * @Author: George Onoufriou <archer>
+ * @Date:   2021-07-08T11:52:22+01:00
+ * @Last modified by:   archer
+ * @Last modified time: 2021-07-08T11:53:03+01:00
+ */
